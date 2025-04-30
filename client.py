912d9281939becdeaf4810fdeae9d725e99b40ae
Felipe Araujo <felp.araupe@gmail.com>
2025-04-29 22:50:37 -0300

    Threads updates in client and server

--- client.py
+++ client.py
@@ -1,20 +1,75 @@
 import socket
+import threading
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 12345))
-payload = 'Hey Server'
 
-try:
-    while True:
-        client_socket.send(payload.encode('utf-8'))
-        data = client_socket.recv(1024)
-        print(str(data))
-        more = input('Want to send more data to the server')
-        if more.lower() == 'y':
-            payload = input('Enter Payload')
-        else:
-            break
-except KeyboardInterrupt:
-    print('Exited by user')
+class Client:
+    def __init__(self, client_socket):
+        self.client_socket = client_socket
 
-client_socket.close()
+    def sendrequest(self, payload):
+        self.client_socket.send(payload.encode('utf-8'))
+
+    def receiverequest(self):
+        while True:
+            data = self.client_socket.recv(1024)
+            print(str(data))
+            more = input('Want to send more data to the server? (y/n) ')
+            if more.lower() == 'y':
+                payload = input('Enter Payload: ')
+                send_thread = threading.Thread(target=self.sendrequest, args=(payload,))
+                send_thread.start()
+            else:
+                break
+
+
+if __name__ == '__main__':
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('127.0.0.1', 12345))
+
+    client = Client(client_socket)
+    initial_payload = 'Hey Server'
+    thread = threading.Thread(target=client.sendrequest, args=(initial_payload,))
+    thread.start()
+    client.receiverequest()
+
+    client_socket.close()
+
+
+# import socket
+# import multiprocessing
+# import threading
+#
+#
+# class Client:
+#     def __init__(self):
+#         pass
+#
+#     @staticmethod
+#     def sendrequest(client_socket, payload):
+#         client_socket.send(payload.encode('utf-8'))
+#
+#     @staticmethod
+#     def receiverequest(client_socket):
+#         while True:
+#             data = client_socket.recv(1024)
+#             print(str(data))
+#             more = input('Want to send more data to the server? (y/n) ')
+#             if more.lower() == 'y':
+#                 payload = input('Enter Payload: ')
+#                 send_thread = threading.Thread(target=Client.sendrequest, args=(client_socket, payload))
+#                 send_thread.start()
+#             else:
+#                 break
+#
+#
+# if __name__ == '__main__':
+#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client_socket.connect(('127.0.0.1', 12345))
+#     payload = 'Hey Server'
+#     thread = threading.Thread(target=Client.sendrequest, args=(client_socket, payload), )
+#     thread.start()
+#     Client.receiverequest(client_socket)
+#
+#     client_socket.close()
+#
+#
--- server.py
+++ server.py
@@ -1,26 +1,38 @@
 import socket
+import threading
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('127.0.0.1', 12345))
-server_socket.listen(5)
-
-while True:
-    print("Server waiting for connection")
-    client_socket, addr = server_socket.accept()
-    print("client connected from", addr)
+def handle_client(client_socket, addr):
+    print(f"Client connected from {addr}")
 
     while True:
         data = client_socket.recv(1024)
         if not data or data.decode('utf-8') == 'END':
             break
 
-        print("received from client client : %s" % data.decode("utf-8"))
-
+        print(f"Received from {addr}: {data.decode('utf-8')}")
         try:
-            client_socket.send(bytes('Hey client', 'utf-8'))
+            client_socket.send(b'Hey client')
         except:
-            print("Exited by the user")
+            print(f"Connection with {addr} closed by the client")
+            break
 
     client_socket.close()
+    print(f"Connection with {addr} closed")
 
-server_socket.close()
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', 12345))
+    server_socket.listen(5)
+    print("Server is running and waiting for connections...")
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+        client_thread.start()
+
+if __name__ == "__main__":
+    main()
+
+
+
