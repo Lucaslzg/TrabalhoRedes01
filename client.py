ac0bdee3777bbd1a633745d3a01dfd301e751b29
Lucaslzg <lucasgallert.aluno@unipampa.edu.br>
2025-05-04 21:50:58 -0300

    Versão com Gui imbutida, coisas a serem mudadas -banco de dados a ser colocado na gui

--- client.py
+++ client.py
@@ -1,54 +1,172 @@
+import sys
 import socket
 import threading
-from response import Response
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class TestClient:
-    def __init__(self, host="localhost", port=12345):
-        self.server_host = host
-        self.server_port = port
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class TestClient(QtCore.QObject):
+    received = QtCore.pyqtSignal(str)
+    connected = QtCore.pyqtSignal(bool)
+
+    def __init__(self):
+        super().__init__()
+        self.client_socket = None
         self.lock = threading.Lock()
+        self.running = False
 
-    def connect(self):
-        self.client_socket.connect((self.server_host, self.server_port))
-        print(f"[CLIENT] Connected to {self.server_host}:{self.server_port}")
+    def connect_to_server(self, host, port):
+        try:
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((host, int(port)))
+            self.running = True
+            self.connected.emit(True)
+            return True
+        except Exception as e:
+            self.connected.emit(False)
+            return False
 
-    def start(self):
-        while True:
-            message = input("[CLIENT] Enter CPF or Name (or type END to quit): ").strip()
-            if not message:
-                continue
-            if message.upper() == "END":
-                self.client_socket.sendall(b"END\n")
-                print("[CLIENT] Disconnected from server.")
-                break
+    def disconnect(self):
+        self.running = False
+        try:
+            self.client_socket.sendall(b"END\n")
+            self.client_socket.close()
+        except Exception:
+            pass
+        self.connected.emit(False)
 
-            try:
-                with self.lock:
-                    self.client_socket.sendall((message + "\n").encode("utf-8"))
-                threading.Thread(target=self.receive_response).start()
-            except Exception as e:
-                print(f"[CLIENT] Error sending data: {e}")
-                break
-
-        self.client_socket.close()
+    def send_message(self, message):
+        if not self.running or not message.strip():
+            return
+        try:
+            with self.lock:
+                self.client_socket.sendall((message.strip() + "\n").encode("utf-8"))
+            threading.Thread(target=self.receive_response, daemon=True).start()
+        except Exception as e:
+            self.received.emit(f"Erro ao enviar: {e}")
+            self.running = False
+            self.connected.emit(False)
 
     def receive_response(self):
         try:
             buffer = ""
-            while True:
-
+            while self.running:
                 data = self.client_socket.recv(1024).decode("utf-8")
                 if not data:
                     break
                 buffer += data
                 if '\n' in buffer:
                     break
-            print(f"\n[CLIENT] Response: {buffer.strip()}")
+            self.received.emit(buffer.strip())
         except Exception as e:
-            print(f"[CLIENT] Error receiving data: {e}")
+            self.received.emit(f"Erro ao receber: {e}")
+            self.running = False
+            self.connected.emit(False)
 
-if __name__ == '__main__':
-    client = TestClient()
-    client.connect()
-    client.start()
+
+class Ui_Janela(object):
+    def setupUi(self, Janela):
+        Janela.setObjectName("Janela")
+        Janela.resize(732, 361)
+        self.centralwidget = QtWidgets.QWidget(Janela)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(10, 10, 711, 321))
+        self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setSpacing(4)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label_2 = QtWidgets.QLabel(self.horizontalLayoutWidget_2)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout_2.addWidget(self.label_2)
+        self.lineEdit = QtWidgets.QLineEdit(self.horizontalLayoutWidget_2)
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setText("localhost")
+        self.verticalLayout_2.addWidget(self.lineEdit)
+        self.label_3 = QtWidgets.QLabel(self.horizontalLayoutWidget_2)
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout_2.addWidget(self.label_3)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.horizontalLayoutWidget_2)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_2.setText("12345")
+        self.verticalLayout_2.addWidget(self.lineEdit_2)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton_2 = QtWidgets.QPushButton(self.horizontalLayoutWidget_2)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.label_4 = QtWidgets.QLabel(self.horizontalLayoutWidget_2)
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_4.setObjectName("label_4")
+        self.horizontalLayout.addWidget(self.label_4)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.label_5 = QtWidgets.QLabel(self.horizontalLayoutWidget_2)
+        self.label_5.setObjectName("label_5")
+        self.verticalLayout_2.addWidget(self.label_5)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.horizontalLayoutWidget_2)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.verticalLayout_2.addWidget(self.lineEdit_3)
+        self.pushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget_2)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout_2.addWidget(self.pushButton)
+        self.horizontalLayout_2.addLayout(self.verticalLayout_2)
+        self.text_resultado = QtWidgets.QTextEdit(self.horizontalLayoutWidget_2)
+        self.text_resultado.setReadOnly(True)
+        self.text_resultado.setObjectName("text_resultado")
+        self.horizontalLayout_2.addWidget(self.text_resultado)
+        Janela.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(Janela)
+        self.statusbar.setObjectName("statusbar")
+        Janela.setStatusBar(self.statusbar)
+
+        self.retranslateUi(Janela)
+        QtCore.QMetaObject.connectSlotsByName(Janela)
+
+        # Logic integration
+        self.client = TestClient()
+        self.client.received.connect(self.display_response)
+        self.client.connected.connect(self.set_connection_status)
+
+        self.pushButton_2.clicked.connect(self.connect_client)
+        self.pushButton.clicked.connect(self.send_query)
+
+    def connect_client(self):
+        ip = self.lineEdit.text()
+        port = self.lineEdit_2.text()
+        self.client.connect_to_server(ip, port)
+
+    def send_query(self):
+        text = self.lineEdit_3.text().strip()
+        if text:
+            self.text_resultado.append(f"[CLIENT] Enviando: {text}")
+            self.client.send_message(text)
+
+    def display_response(self, message):
+        self.text_resultado.append(f"[SERVER] {message}")
+
+    def set_connection_status(self, status):
+        if status:
+            self.label_4.setText("Conectado")
+        else:
+            self.label_4.setText("Desconectado")
+
+
+    def retranslateUi(self, Janela):
+        _translate = QtCore.QCoreApplication.translate
+        Janela.setWindowTitle(_translate("Janela", "Client"))
+        self.label_2.setText(_translate("Janela", "IP do Servidor:"))
+        self.label_3.setText(_translate("Janela", "Porta:"))
+        self.pushButton_2.setText(_translate("Janela", "Connectar"))
+        self.label_4.setText(_translate("Janela", "Desconectado"))
+        self.label_5.setText(_translate("Janela", "CPF ou Nome a ser Consultado:"))
+        self.pushButton.setText(_translate("Janela", "Consultar"))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Janela = QtWidgets.QMainWindow()
+    ui = Ui_Janela()
+    ui.setupUi(Janela)
+    Janela.show()
+    sys.exit(app.exec_())
--- server.py
+++ server.py
@@ -1,14 +1,19 @@
-import json
+import sys
 import os
 import socket
 import sqlite3
 import threading
+import json
 from multiprocessing import Process, freeze_support
+from PyQt5 import QtCore, QtWidgets
 
+# Caminho fixo para o banco de dados
+DB_PATH = r"C:\Users\LucasZGallert\Documents\Cod\Gersu\basecpf.db"
+
+# Dependências externas esperadas
 from response import Response
 from user import User
 
-DB_PATH = "C:/Redes/basecpf.db"
 
 class Server:
     def __init__(self, server_socket):
@@ -32,7 +37,6 @@ class Server:
         response_json = Response(users).toJson()
         print("[THREAD] JSON →", response_json)
         return response_json
-
 
     def handle_request(self, client_socket, message):
         json_payload = self.database_access(message)
@@ -76,10 +80,85 @@ class Server:
                 break
 
 
+class Ui_Janela(object):
+    def setupUi(self, Janela):
+        Janela.setObjectName("Janela")
+        Janela.resize(331, 212)
+        self.centralwidget = QtWidgets.QWidget(Janela)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(20, 0, 291, 91))
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setSpacing(2)
+        self.label_2 = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label_2.setText("IP do Servidor:")
+        self.verticalLayout_2.addWidget(self.label_2)
+        self.lineEdit = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.verticalLayout_2.addWidget(self.lineEdit)
+        self.label = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.verticalLayout_2.addWidget(self.label)
+        self.label_3 = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label_3.setText("Porta:")
+        self.verticalLayout_2.addWidget(self.label_3)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.verticalLayout_2.addWidget(self.lineEdit_2)
+
+        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(40, 90, 251, 41))
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
+        self.pushButton = QtWidgets.QPushButton(self.verticalLayoutWidget_2)
+        self.pushButton.setText("Iniciar/Parar Servidor")
+        self.verticalLayout_3.addWidget(self.pushButton)
+
+        self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(110, 130, 111, 41))
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_3)
+        self.label_4 = QtWidgets.QLabel(self.verticalLayoutWidget_3)
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_4.setText("Parado")
+        self.verticalLayout_4.addWidget(self.label_4)
+
+        Janela.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(Janela)
+        Janela.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(Janela)
+        Janela.setStatusBar(self.statusbar)
+
+        self.pushButton.clicked.connect(self.toggle_server)
+        self.server_socket = None
+        self.server_thread = None
+
+    def toggle_server(self):
+        if self.server_thread:
+            self.label_4.setText("Parado")
+            print("[GUI] Servidor encerrado.")
+            # Not safe to close socket here forcibly if thread is still using it
+            return
+        try:
+            host = self.lineEdit.text() or "0.0.0.0"
+            port = int(self.lineEdit_2.text() or 12345)
+            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server_socket.bind((host, port))
+            self.server_socket.listen(1000)
+            self.server_thread = threading.Thread(target=self.run_server, daemon=True)
+            self.server_thread.start()
+            self.label_4.setText("Rodando")
+            print(f"[GUI] Servidor iniciado em {host}:{port}")
+        except Exception as e:
+            print(f"[GUI] Erro ao iniciar servidor: {e}")
+            self.label_4.setText("Erro")
+
+    def run_server(self):
+        freeze_support()
+        server = Server(self.server_socket)
+        server.connection()
+
+
 if __name__ == "__main__":
-    freeze_support()
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('0.0.0.0', 12345))
-    server_socket.listen(1000)
-    server = Server(server_socket)
-    server.connection()
+    app = QtWidgets.QApplication(sys.argv)
+    Janela = QtWidgets.QMainWindow()
+    ui = Ui_Janela()
+    ui.setupUi(Janela)
+    Janela.show()
+    sys.exit(app.exec_())
